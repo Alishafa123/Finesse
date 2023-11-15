26dfc0066ef874a501c26e5965194485c3d49be6
Ali Shafa <116679960+Alishafa123@users.noreply.github.com>
2023-11-15 17:06:33 +0500

    Footer Added

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,15 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 import Header from './components/header';
 import Home  from './components/home';
+import Footer  from './components/Footer';
+
 
 
 import './styles/App.scss';
 import './styles/header.scss';
 import './styles/home.scss';
+import './styles/footer.scss';
+
 
 
 
@@ -19,6 +23,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes>
+    <Footer></Footer>
    </Router>
    
    
---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>FINEESSE</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.facebook.com/choudhary.ali.5836" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://www.instagram.com/ali_shafa007/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Alishafa123" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
